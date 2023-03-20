5522804e35ab63f3180468fa698f46e26bc655f5
Darwin1946 <darwin931369866@gmail.com>
2023-03-20 22:58:28 +0800

    compare jni and naive java

--- Java2cpp.java
+++ Java2cpp.java
@@ -1,4 +1,10 @@
 public class Java2cpp {
+    public static int add(int a,int b)
+    {
+        int res = 0;
+        res = a+b;
+        return res;
+    }
     public native int DLL_ADD(int a, int b);
     public native int DLL_SUB(int a, int b);
     public native int DLL_MUL(int a, int b);
@@ -7,6 +13,20 @@ public class Java2cpp {
         System.loadLibrary("javaCallcpp");
         int sum = 0;
         Java2cpp test = new Java2cpp();
-        sum = test.DLL_ADD(2, 4);
-        System.out.println("Java call cpp dll result:" + sum);}
+        long startTime = System.currentTimeMillis();
+        int loopTime = 100000000;
+        for(int i=0;i<loopTime;i++){
+        sum += test.DLL_ADD(i, i+1);
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("jni time used:"+(endTime-startTime));
+
+        long startTime2 = System.currentTimeMillis();
+        for(int i=0;i<loopTime;i++){
+        sum += add(i,i+1);
+        }
+        long endTime2 = System.currentTimeMillis();
+        System.out.println("java time used:"+(endTime2-startTime2));
+        // System.out.println("Java call cpp dll result:" + sum);
+    }
     }
